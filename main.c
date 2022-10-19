0fa446d2df3644fa5f4f3442567efc6ff48ae97e
Kunal <85743894+kunalzade-beta@users.noreply.github.com>
2022-10-19 21:54:01 +0530

    First commit

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main()
 {
-    printf("yo Bro");
+    printf("Hii I am kunal");
 }
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("this is project repo");
+        System.out.println("YO i am kunal");
     }
 }
